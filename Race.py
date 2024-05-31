c5417b5dfc281cd34bfd0e0e70a8d325d0e0b96f
R33etro <piotrek.kulej4@gmail.com>
2024-05-31 14:37:18 +0200

    temp

--- Race.py
+++ Race.py
@@ -16,7 +16,14 @@ class Race:
     #     # czy nie lepiej liste aut z czasem rzeczywistym - zdjęcie w sms
     #     return
 
-    def setTrack(self, track):
+    def getBolideStats():
+        return
+
+    def getPitstopTimeLost():
+        return 
+
+    def raceCalculations(self, track, numberOfLaps, numberOfCars):
+
         if(track == 1): #SPA
             RE = RandomEvents.RandomEvents(0.64, 0.49)
             PsTL = PitstopTimeLost.PitstopTimeLost(19.1, 18.5, 14.2, RE)
@@ -24,14 +31,6 @@ class Race:
             RE = RandomEvents.RandomEvents(0.35, 0.21)
             PsTL = PitstopTimeLost.PitstopTimeLost(17.7, 16.9, 15.3, RE)
 
-    def getBolideStats():
-        return
-
-    def getPitstopTimeLost():
-        return 
-
-    def raceCalculations(self, PsTL, RE, numberOfLaps, numberOfCars):
-        
         # randomowe eventy występują i losują się na jedno okrążenie
         # deszcz odejmuje bolide.skill
         # safety car odejmuje bolide.strategy
