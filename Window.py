e60b0d2f0e5deca3e2a325676909f8747c492f0c
R33etro <piotrek.kulej4@gmail.com>
2024-06-04 16:21:45 +0200

    jadą prosto

--- Window.py
+++ Window.py
@@ -10,9 +10,9 @@ clock = pygame.time.Clock()
 track = Track(screen_width, screen_height, screen, track_width, track_height, margin)
 
 #creating bolides
-bolide1 = Bolide("Ferrari", 5, 1, 1, (255, 0, 0), screen, track.track_x - 50, track.track_y)
-bolide2 = Bolide("Mercedes", 5, 1, 1, (192, 192, 192), screen, track.track_x - 100, track.track_y + 100)
-bolide3 = Bolide("RedBull", 5, 1, 1, (0, 0, 255), screen, track.track_x + 150, track.track_y + 150)
+bolide1 = Bolide("Ferrari", 4, 1, 1, (255, 0, 0), screen, track.track_x - 70, track.track_y - 25)
+bolide2 = Bolide("Mercedes", 3, 1, 1, (192, 192, 192), screen, track.track_x - 70, track.track_y - 50)
+bolide3 = Bolide("RedBull", 5, 1, 1, (0, 0, 255), screen, track.track_x - 70, track.track_y - 75)
 
 screen.fill((50,50,50))
 track.draw_track()
